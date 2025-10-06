3fbefbc0832452fedcdd90e690f8313594971e85
imupset27 <58505594+imupset27@users.noreply.github.com>
2025-10-06 12:17:07 +0800

    Update app.R

--- app.R
+++ app.R
@@ -339,5 +339,5 @@ server <- function(input, output, session) {
   )
 }
 
-auth0::shinyAppAuth0(ui = auth0_ui(ui), server = auth0_server(server))
+auth0::shinyAppAuth0(ui = ui, server = server)
 #shinyApp(ui = ui, server = server)
